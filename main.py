c7531c9d2a98452c316611b955dcd290090d8898
Sharl0tteIsTaken <jamesly2013@gmail.com>
2026-02-06 17:58:53 +0800

    ENH: add Chinese language
    - Now support Traditional Chinese.
    - Support language switching.

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ The source code of the server.
 
 import os
 import smtplib
+from typing import Literal
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_bootstrap import Bootstrap5  # type: ignore[import-untyped, note]
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import JSON
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from demo_morse_code_converter.converter import Converter
@@ -15,6 +17,9 @@ from demo_tic_tac_toe.showmaker_demo import ShowMaker
 
 # ---------------------------------------------------------------------
 
+type LanguageCode = Literal["Eng", "ZhTw"]
+
+LANGUAGES: list[LanguageCode] = ["Eng", "ZhTw"]
 MAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 MAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 APP_KEY = os.getenv("APP_SECRET_KEY")
@@ -26,66 +31,77 @@ assert isinstance(APP_KEY, str), f"Environment variable {APP_KEY=}"
 assert isinstance(SQL_DB_URI, str), f"Environment variable {SQL_DB_URI=}"
 
 
-# setup flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = APP_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = SQL_DB_URI
-
-
 class Base(DeclarativeBase):
     """Base model for SQLAlchemy."""
 
 
+# setup flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = APP_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = SQL_DB_URI
 db = SQLAlchemy(model_class=Base)
-
-# init db & bootstrap
-db.init_app(app)
-Bootstrap5(app)
 
 
 # db table
-class AllProjectDemo(db.Model):  # type: ignore[name-defined]
+class Project(Base):  # type: ignore[name-defined]
     """
     Subclass db.Model to define a model class.
     The model will generate a table name by converting the CamelCase
     class name to snake_case.
     """
-    apd_id: Mapped[int] = mapped_column(primary_key=True)
-    apd_title: Mapped[str] = mapped_column(nullable=False, unique=True)
-    apd_desc: Mapped[str] = mapped_column(nullable=False)
+    __tablename__ = "Project"
+    info_id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False, unique=True)
+    desc_eng: Mapped[str] = mapped_column(nullable=False)
+    desc_zhtw: Mapped[str] = mapped_column(nullable=False)
+    keyword_eng: Mapped[list] = mapped_column(JSON, nullable=False)
+    keyword_zhtw: Mapped[list] = mapped_column(JSON, nullable=False)
 
-    # value hint (apd_preview): video, image or both
-    apd_preview: Mapped[str] = mapped_column(nullable=False)
-    apd_videos: Mapped[str] = mapped_column()
-    apd_images: Mapped[str] = mapped_column()
+    # value hint (preview_type): video, image, both, or none
+    preview_type: Mapped[str] = mapped_column(nullable=True)
+    preview_video: Mapped[str] = mapped_column(nullable=True)
+    preview_image: Mapped[str] = mapped_column(nullable=True)
 
-    # value (apd_is_demo): True or False
-    apd_is_demo: Mapped[str] = mapped_column(nullable=False)
+    # value (is_demo): True or False
+    is_demo: Mapped[str] = mapped_column(nullable=False)
 
-    # value (apd_demo_ep): endpoint for demo, use at index.html
-    apd_demo_ep: Mapped[str] = mapped_column()
-    apd_github_link: Mapped[str] = mapped_column(nullable=False)
+    # value (demo_ep): endpoint for demo, use at index.html
+    demo_ep: Mapped[str] = mapped_column(nullable=True)
+    gh_link: Mapped[str] = mapped_column(nullable=False)
+    gh_date: Mapped[str] = mapped_column(nullable=False)
+
+
+class Current():
+    language: LanguageCode = "Eng"
+    endpoint: str = "home"
+
+    def switch_language(self):
+        self.language = "Eng" if self.language == "ZhTw" else "ZhTw"
+
+    def switch_endpoint(self):
+        if request.endpoint != "switch_language":
+            self.endpoint = request.endpoint  # pyright: ignore
 
 
 # website routes
 @app.route('/')
 def home() -> str:
     """The home page of website."""
-    db_data = db.session.execute(db.select(AllProjectDemo)).scalars().all()
+    db_data = db.session.execute(db.select(Project)).scalars().all()
     results = db.session.execute(
-        db.select(AllProjectDemo).where(AllProjectDemo.apd_preview == 'image')
+        db.select(Project).where(Project.preview_type == 'image')
         ).scalars().all()
     if results != []:
         image_locs: dict[int, list] = {}
         first_loc: dict[int, str] = {}
         for project in results:
-            locations = project.apd_images.split(", ")
+            locations = project.preview_image.split(", ")
             image_loc = []
             for loc in locations:
                 image_loc.append(loc)
-            first_loc[project.apd_id] = image_loc[0]
+            first_loc[project.info_id] = image_loc[0]
             del image_loc[0]
-            image_locs[project.apd_id] = image_loc
+            image_locs[project.info_id] = image_loc
         image_nums = range(len(image_locs))
     else:
         first_loc = {}
@@ -93,6 +109,8 @@ def home() -> str:
         image_nums = range(0)
     return render_template(
         "index.html",
+        # pylint: disable-next=possibly-used-before-assignment
+        language=current.language,
         db_data=db_data,
         first_loc=first_loc,
         image_locs=image_locs,
@@ -104,17 +122,36 @@ def home() -> str:
 @app.route("/about")
 def about():
     """The about page of website."""
-    return render_template("about.html", page="about")
+    current.switch_endpoint()
+    return render_template(
+        "about.html",
+        language=current.language,
+        page="about")
 
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     """The contact page of website."""
+    current.switch_endpoint()
     if request.method == "POST":
         data = request.form
         send_email(data["name"], data["email"], data["message"])
         return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False, page="contact")
+    return render_template(
+        "contact.html",
+        language=current.language,
+        msg_sent=False,
+        page="contact"
+        )
+
+
+@app.route("/switch-language")
+def switch_language():
+    current.switch_endpoint()
+    current.switch_language()
+    return redirect(url_for(
+        current.endpoint  # pyright: ignore[reportArgumentType]
+        ))
 
 
 @app.route("/gate/tic-tac-toe")
@@ -123,7 +160,8 @@ def gate_tic_tac_toe():
     The page to redirect to tic tac toe demo.
     This also resets the demo.
     """
-    dk_showmaker.initiate()  # pylint: disable=possibly-used-before-assignment
+    # pylint: disable-next=possibly-used-before-assignment
+    dk_showmaker.initiate()
     dk_showmaker.new_game()
     return redirect(url_for('demo_tic_tac_toe'))
 
@@ -233,7 +271,12 @@ def send_email(name: str, email: str, message: str) -> None:
 
 
 if __name__ == "__main__":
+    # init db & bootstrap
+    db.init_app(app)
+    Bootstrap5(app)
+
     dk_showmaker = ShowMaker()
     dk_showmaker.new_game()
     converter = Converter()
+    current = Current()
     app.run(debug=True, port=5000)
